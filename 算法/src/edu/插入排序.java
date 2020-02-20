94fde128f8a581d9f8a455e58219a309043b8dc6
ku-mao <1335446763@qq.com>
2020-02-20 18:33:02 +0800

    插入排序

---
+++ 算法/src/edu/插入排序.java
@@ -0,0 +1,28 @@
+package edu;
+
+public class 插入排序 {
+    public static void main(String[] args) {
+        int a[] = {4,65,21,3,45,5};
+        for(int index = 0 ; index < a.length -1 ; index++){ //已排好数组的最后一个元素的索引
+            int temp = a[index+1];//待插入元素的值
+            int insert_index = index + 1;//待插入位置的索引,必须加1，因为可能会不交换
+
+            //找到待插入位置的索引
+            for(int i = index ; i >= 0 ; i-- ){
+                if(temp < a[i]){
+                    insert_index = i;
+                }else {
+                    break;
+                }
+            }
+            //后移插入元素后面的元素
+            for(int j = index ; j >= insert_index ; j--){
+                a[j+1] = a [j];
+            }
+                a[insert_index] = temp;
+        }
+        for(int i = 0 ; i < a.length ; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
