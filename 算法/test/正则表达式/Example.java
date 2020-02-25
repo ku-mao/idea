11aa793330648b27b90c28b8eae591f947cafe73
ku-mao <1335446763@qq.com>
2020-02-25 21:37:03 +0800

    分组替换 Matcher和Pattern对象的作用

--- 算法/test/正则表达式/Example.java
+++ 算法/test/正则表达式/Example.java
@@ -3,6 +3,9 @@ package 正则表达式;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Example {
 
@@ -43,5 +46,79 @@ public class Example {
         System.out.println(str.replaceAll("-|,|_|\\|"," "));
     }
 
-    
+    /**
+     * 非贪婪匹配
+     *
+     *    /*这是一个主函数*/
+     /**   void main(){
+      *    /* 这是主函数内部*/
+    /**       int a = 10 ;//声明一个变量a
+     *       int b = 20 ;//声明一个变量b
+     *       printf("a + b = %d", a + b);
+     *   }
+      *   quit
+     */
+    @Test
+    public void test4(){
+        Scanner s = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        sb.append("/*这是一个主函数*/")
+          .append("void main(){")
+          .append("/* 这是主函数内部*/\n")
+          .append("int a = 10 ;//声明一个变量a\n")
+          .append("int b = 20 ;//声明一个变量b\n")
+          .append("printf(\"a + b = %d\", a + b);\n" )
+          .append("}\n");
+//         String temp;
+//         while(!(temp = s.nextLine()).equals("quit")){
+//             sb.append(temp).append("\n");
+//         }
+        String str = sb.toString();
+
+        String regex1 = "\\/\\*.+?\\*\\/";
+        str = str.replaceAll(regex1,"");
+        regex1 = "\\/\\/.+?\\n";
+        System.out.println(str.replaceAll(regex1,"\n"));
+    }
+
+    @Test
+    public void test5(){
+    String regex = "%.*?%";//非贪婪匹配
+        String str = "%afgs% dfff %fsf% sfs%";
+        System.out.println(str.replaceAll(regex,"aaa"));
+        System.out.println("*****************");
+        String regex1 = "%.+%";//贪婪匹配
+        String str1 = "%afgs% dfff %fsf sfs%";
+        System.out.println(str.replaceAll(regex1,"aaa"));
+    }
+
+    /**
+     * 分组替换
+     */
+    @Test
+    public void test6(){
+       String regex = "(%)(.*?)(%)";//引号里面全部为组0($0)
+        String str = "%good% %study% %oh%";
+        System.out.println(str.replaceAll(regex,"$1"));//"$2"代表把组2直接拿出来
+        System.out.println(str.replaceAll(regex,"$1dai$3"));
+    }
+
+    /**
+     * 正则表达式里面的2个对象
+     * Matcher//把匹配的字符串遍历出来
+     * Pattern//模式对象  解析规则
+     */
+    @Test
+    public void test7(){
+        Pattern pattern = Pattern.compile("(%)(.*?)(%)");
+        String str = "%good% %study% %oh%";
+        Matcher m = pattern.matcher(str);
+        while(m.find()){
+           // System.out.println(m.group()+ "startIndex:" + m.start() +  ",endIndex:" + m.end());//全部输出，group默认参数为0
+           // System.out.println(m.group(1)+"dai"+ m.group(3));//替换百分号中间的字符
+            System.out.println(m.group(2).substring(1,2));//取出中间字符的部分输出结果为go,st,oh
+
+        }
+
+    }
 }
