e89825b3f1622d093d0ced65af3157a23ad54216
ku-mao <1335446763@qq.com>
2020-02-26 20:49:10 +0800

    集合的填加、查找和删除

---
+++ 算法/src/edu/集合.java
@@ -0,0 +1,63 @@
+package edu;
+
+class ArrayList{
+    int length = 10;
+    int[] data = new int[length];
+    int size = 0;
+
+    //添加元素
+    public void add(int d){
+        if(size < length){
+            this.data[size] = d;
+        }else {
+            length *= 2;
+            int[] a = new int[length];
+            for(int i = 0 ; i < size ; i++ ){
+                a[i] = data[i];
+            }
+            data = a;
+            data[size] = d;
+        }
+        size++;
+    }
+
+    //查找元素
+    public  Integer get(int i ){
+        if( 0 > i || i >= size){
+            return null;
+        }
+        return data[i];
+    }
+
+    //删除元素
+    public void remove(int i){
+        if( 0 > i || i >= size){
+            return ;
+        }
+        for( int t = i ; t < length - 1 ; t++){
+            data[i] = data[i + 1];
+        }
+        size--;
+    }
+}
+public class 集合 {
+    public static void main(String[] args) {
+       ArrayList a = new ArrayList();
+        a.add(2);//可以无限加
+        a.add(222);
+        a.add(52);
+        a.add(552);
+        a.add(21);
+        a.add(12);
+        a.add(0);
+        a.add(76);
+        a.add(2);
+        a.add(2);
+        a.add(2);
+        a.add(2);
+        a.add(4);
+        System.out.println(a.get(4));
+        a.remove(4);
+        System.out.println(a.get(4));
+    }
+}
