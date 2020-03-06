5ac44bc9c9aa4892c71389c1eebd3c5dfea42446
ku-mao <1335446763@qq.com>
2020-03-06 12:58:58 +0800

    非递归解决汽水瓶问题

--- everyday/src/edu/汽水瓶.java
+++ everyday/src/edu/汽水瓶.java
@@ -1,22 +1,28 @@
 package edu;
 import java.util.*;
 public class 汽水瓶 {
-        public static void main(String[] args){
-            Scanner s = new Scanner(System.in);
-            while(s.hasNext()){
-                int n = s.nextInt();
-                System.out.println(bottle(n));
+    public static int bottle(int n) {
+        int total = 0;//饮料总数，当大于两个空瓶的时候才可以换饮料
+        while(n > 2) {
+            //n/3-->能够换来的饮料
+            total = total+n/3; //新的空瓶子个数--n%3可能会不能整除
+            n = n/3 + n%3;
+         }
+         //如果n==2，说明有两个空瓶可以喝商家借一瓶饮料
+         if(n == 2) {
+             total = total + 1;
+         }
+         return total;
+    }
+      public static void main(String[] args){
+                 Scanner sc=new Scanner(System.in);
+                 int n;
+                 while(sc.hasNext()) {
+                    n=sc.nextInt();
+                    System.out.println(bottle(n));
+                    if(n==0){
+                        return;
+                    }
             }
-        }
-        
-        public  static int bottle(int n){
-            if (n <= 1) return 0;
-            if(n == 2) return 1;
-            if(n == 3) return 1;
-            int num = 0;
-            num = n / 3;
-            return num + bottle(n-3*num + num);
-
-        }
-
+    }
 }
