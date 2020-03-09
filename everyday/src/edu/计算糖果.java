fb8cfac44a0e2c0cf778db9e19aa21405455063c
ku-mao <1335446763@qq.com>
2020-03-09 21:31:11 +0800

    计算糖果

---
+++ everyday/src/edu/计算糖果.java
@@ -0,0 +1,23 @@
+package edu;
+import  java.util.Scanner;
+public class 计算糖果 {
+        public static void main(String[] args){
+            Scanner s = new Scanner(System.in);
+            int a = s.nextInt();
+            int b = s.nextInt();
+            int c = s.nextInt();
+            int d = s.nextInt();
+            if((a+c)%2 == 0 && (a+c)>=0 && (b+d)%2 == 0 && (b+d)>=0){
+                int A = (a+c)/2;
+                int B = (b+d)/2;
+                if((d - B) >= 0) {
+                    int C = d - B;
+                    System.out.print(A + " " + B + " " + C);
+                    return;
+                }
+            }
+            System.out.println("No");
+
+        }
+}
+
