ecf0dfadaa7ca3fc1f2f65f05ce8a5f51fe3554d
ku-mao <1335446763@qq.com>
2020-03-09 13:46:01 +0800

    希尔排序的修改和堆排序

--- 算法/src/edu/sort/希尔排序.java
+++ 算法/src/edu/sort/希尔排序.java
@@ -4,32 +4,28 @@ public class 希尔排序 {
 
     public static void main(String[] args) {
 
-        int[] data = new int[] { 5, 3, 6, 2, 1, 9, 4, 8, 7 };
-        shellSort(data);
+        int[] a = new int[] { 5, 3, 6, 2, 1, 9, 4, 8, 7 };
+        shellSort(a);
     }
 
-    public static void shellSort(int[] data) {
+    public static void shellSort(int[] a) {
         // 计算出最大的h值
         int h = 1;
-        while (h <= data.length / 3) {
+        while (h <= a.length / 3) {
             h = h * 3 + 1;
         }
-        while (h > 0) {
-            for (int i = h; i < data.length; i += h) {
-                if (data[i] < data[i - h]) {
-                    int tmp = data[i];
-                    int j = i - h;
-                    while (j >= 0 && data[j] > tmp) {
-                        data[j + h] = data[j];
-                        j -= h;
+        for (; h >= 1; h /= 3) {
+            for (int i = 0; i < a.length - h; i += h) {
+                for (int j = i + h; j > 0; j -= h) {
+                    if (a[j] < a[j - h]) {
+                        int temp = a[j];
+                        a[j] = a[j - h];
+                        a[j - h] = temp;
                     }
-                    data[j + h] = tmp;
                 }
             }
-            // 计算出下一个h值
-            h = (h - 1) / 3;
         }
-        for( int i : data){
+        for( int i : a){
             System.out.print(i+" ");
         }
     }
