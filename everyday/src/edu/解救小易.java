fd65b728c1bb9733ceae208b2c829ca4aaf1e870
ku-mao <1335446763@qq.com>
2020-03-11 14:26:44 +0800

    有一片1000*1000的草地，小易初始站在(1,1)(最左上角的位置)。小易在每一秒会横向或者纵向移动到相邻的草地上吃草(小易不会走出边界)。大反派超超想去捕捉可爱的小易，他手里有n个陷阱。第i个陷阱被安置在横坐标为xi，纵坐标为yi的位置上，小易一旦走入一个陷阱，将会被超超捕捉。你为了去解救小易，需要知道小易最少多少秒可能会走入一个陷阱，从而提前解救小易

---
+++ everyday/src/edu/解救小易.java
@@ -0,0 +1,28 @@
+package edu;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 解救小易 {
+    public static void main(String[] agrs) throws Exception{
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        String str = br.readLine();
+        int num = Integer.parseInt(str);
+        String[] s1 = br.readLine().split(" ");
+        String[] s2 = br.readLine().split(" ");
+        int[] x = new int[num];
+        int[] y = new int[num];
+        for(int i = 0; i < num; i++){
+            x[i] = Integer.parseInt(s1[i]);
+            y[i] = Integer.parseInt(s2[i]);
+        }
+        int min = x[0] - 1 + y[0] - 1;
+        for(int i = 1; i < num; i++){
+            int temp = x[i] - 1 + y[i] - 1;
+            if(temp < min){
+                min = temp;
+            }
+        }
+        System.out.print(min);
+    }
+}
