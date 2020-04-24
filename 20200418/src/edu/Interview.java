413a45afbf908707fc193c3715289c56ce3a0133
ku-mao <1335446763@qq.com>
2020-04-24 22:27:07 +0800

    二叉树的遍历 给定带有空格的先序遍历结果,输出不带空格的中序遍历结果

---
+++ 20200418/src/edu/Interview.java
@@ -0,0 +1,57 @@
+package edu;
+
+import java.util.Scanner;
+
+public class Interview {
+    public static class TreeNode {
+        char val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(char x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            //先根据先序遍历构建树
+            TreeNode root = build(sc.nextLine());
+            //再进行中序遍历
+            inOrder(root);
+            System.out.println();
+        }
+    }
+
+    private static int index;
+    private static TreeNode build(String line) {
+        index = 0;
+        return createTree(line);
+    }
+
+    private static TreeNode createTree(String line) {
+        char c = line.charAt(index);
+        if(c == '#') {
+            return null;
+        }
+        //如果结点非空,就访问这个结点,也就是创建这个结点
+        TreeNode node  = new TreeNode(c);
+        index++;
+        node.left = createTree(line);
+        index++;
+        node.right = createTree(line);
+        return node;
+    }
+
+    private static void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
+}
+
+
