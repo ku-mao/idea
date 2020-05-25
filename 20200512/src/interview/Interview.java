801df6c5d87ef6d6d646632527704281e0b539d1
ku-mao <1335446763@qq.com>
2020-05-25 20:03:38 +0800

    找出2个只出现1次的数字,其他均出现2次

--- 20200512/src/interview/Interview.java
+++ 20200512/src/interview/Interview.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Interview {
+    /**
+     * 找出只出现一次的数字,其他数字均出现2次
+     * @param nums
+     * @return
+     */
     public static int singleNumber(int[] nums) {
         //key 是具体的数字, value 是该数字出现的次数
         Map<Integer, Integer> map = new HashMap <>();
@@ -23,8 +28,61 @@ public class Interview {
         return 0;
     }
 
+    /**
+     * 对上面算法的改进
+     * @param nums
+     * @return
+     */
+    public static int singleNumber2(int[] nums) {
+        int ret = 0;
+        for(int x : nums) {
+            ret ^= x;
+        }
+        return ret;
+    }
+
+    /**
+     * 给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。
+     * 找出只出现一次的那两个元素
+     * @param nums
+     * @return
+     */
+    public static int[] singleNumber3(int[] nums) {
+        int ret = 0;
+        for (int x : nums) {
+            ret ^= x;
+        }
+
+        //找到ret中bit位不为0的那个bit位
+        int bit = 0;
+        for(; bit < 32; bit++) {
+            if((ret & (1 << bit)) > 0) {
+                break;
+            }
+        }
+        //此时bit对应的位就是1
+        int a = 0;
+        int b = 0;
+        for (int x : nums) {
+            //对数组中的元素按照那个bit是 0 和 不是0 分组
+            if ((x & (1 << bit)) > 0) {
+                a ^= x;
+            } else {
+                b ^= x;
+            }
+        }
+        return new int[]{a, b};
+    }
+
     public static void main(String[] args) {
         int[] array = {1, 4, 3, 3, 4};
         System.out.println(singleNumber(array));
+        System.out.println(singleNumber2(array));
+
+        int[] array2 = {1, 1, 4, 5, 5, 4, 6, 8};
+        int[] ret = singleNumber3(array2);
+        for (int x : ret) {
+            System.out.print(x + " ");
+        }
     }
 }
