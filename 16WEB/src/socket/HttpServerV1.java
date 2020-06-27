87c2ed1b4a170dac6331b1183380b5b87f847b2c
ku-mao <1335446763@qq.com>
2020-06-27 23:36:22 +0800

    实现http协议的服务器

---
+++ 16WEB/src/socket/HttpServerV1.java
@@ -0,0 +1,37 @@
+package socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServerV1 {
+    public ServerSocket serverSocket= null;
+
+    public HttpServerV1(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+    public void start() throws IOException {
+        System.out.println("服务器启动");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    process(clientSocket);
+                }
+            });
+        }
+
+    }
+
+    private void process(Socket clientSocket) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
