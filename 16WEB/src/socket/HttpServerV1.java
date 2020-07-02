0f4915e90f2eb1397c6d177bc880cdb0ff37a922
ku-mao <1335446763@qq.com>
2020-07-02 22:10:08 +0800

    完善代码

--- 16WEB/src/socket/HttpServerV1.java
+++ 16WEB/src/socket/HttpServerV1.java
@@ -1,8 +1,10 @@
 package socket;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -28,6 +30,31 @@ public class HttpServerV1 {
     }
 
     private void process(Socket clientSocket) {
+        //获取字符流
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+
+            //1.读取请求并解析
+            //   a)解析首行
+            String firstLine = bufferedReader.readLine();
+            String[] firstLineTokens = firstLine.split(" ");
+            String method = firstLineTokens[0];
+            String url = firstLineTokens[1];
+            String version = firstLineTokens[2];
+            //   b)解析header
+            Map<String, String> headers = new HashMap <>();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null && line.length() != 0 ) {
+                String[] headerToken = line.split(": ");
+                headers.put(headerToken[0], headerToken[1]);
+            }
+            //   c)解析body(暂时先不考虑)
+            //2.根据请求计算响应
+            //3.把响应写回给客户端
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
