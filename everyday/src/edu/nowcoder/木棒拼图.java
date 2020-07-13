304a1dcef8538c75dffb31b6fd13872f8ae60160
ku-mao <1335446763@qq.com>
2020-07-13 21:29:46 +0800

    用给出的木棒拼图

---
+++ everyday/src/edu/nowcoder/木棒拼图.java
@@ -0,0 +1,39 @@
+package edu.nowcoder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 最长的 > 除过最长的其余长的的和 就可以组成简单多边形
+ */
+public class 木棒拼图 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList <>();
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int type = sc.nextInt();
+                int L = sc.nextInt();
+                if(type == 1) {
+                    list.add(L);
+                } else if (list.contains(L)) {
+                    list.remove((Object)L);
+                }
+            }
+            Collections.sort(list);
+            int maxLen = list.get(list.size() - 1);
+            int sumLen = 0;
+            for (int i = 0; i < list.size() - 1; i++) {
+                sumLen += list.get(i);
+            }
+            if (maxLen > sumLen) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
