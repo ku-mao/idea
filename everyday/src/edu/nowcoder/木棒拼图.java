ace29b154745edb0fb448d23a3bc861b1f3a5362
ku-mao <1335446763@qq.com>
2020-07-18 11:11:50 +0800

    修改代码

--- everyday/src/edu/nowcoder/木棒拼图.java
+++ everyday/src/edu/nowcoder/木棒拼图.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * 最长的 > 除过最长的其余长的的和 就可以组成简单多边形
+ * 最长的 < 除过最长的其余长的的和 就可以组成简单多边形
  */
 public class 木棒拼图 {
     public static void main(String[] args) {
@@ -14,25 +14,26 @@ public class 木棒拼图 {
         List<Integer> list = new ArrayList <>();
         while (sc.hasNext()) {
             int n = sc.nextInt();
-            for (int i = 0; i < n; i++) {
+            while (n-- > 0) {
                 int type = sc.nextInt();
                 int L = sc.nextInt();
-                if(type == 1) {
+                if (type == 1) {
                     list.add(L);
                 } else if (list.contains(L)) {
-                    list.remove((Object)L);
+                    list.remove((Object) L);
                 }
-            }
-            Collections.sort(list);
-            int maxLen = list.get(list.size() - 1);
-            int sumLen = 0;
-            for (int i = 0; i < list.size() - 1; i++) {
-                sumLen += list.get(i);
-            }
-            if (maxLen > sumLen) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
+
+                Collections.sort(list);
+                int maxLen = list.get(list.size() - 1);
+                int sumLen = 0;
+                for (int i = 0; i < list.size() - 1; i++) {
+                    sumLen += list.get(i);
+                }
+                if (maxLen < sumLen) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
             }
         }
     }
