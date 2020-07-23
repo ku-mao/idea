47297cb39586ea1ec55369dc16291e0728c69591
ku-mao <1335446763@qq.com>
2020-07-23 22:01:35 +0800

    字符串的应用

---
+++ everyday/src/edu/nowcoder/查找兄弟单词.java
@@ -0,0 +1,43 @@
+package edu.nowcoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class 查找兄弟单词 {
+    private static ArrayList<String> broList = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String[] dict = scan.nextLine().split(" ");
+            int n = Integer.parseInt(dict[0]);
+            String target = dict[n + 1];
+            int index = Integer.parseInt(dict[dict.length - 1]);
+            for (int i = 1; i <= n; i++) {
+                if (dict[i].length() == target.length()
+                        && !target.equals(dict[i])
+                        && isBrother(dict[i], target)) {
+                    broList.add(dict[i]);
+                }
+            }
+            Collections.sort(broList);
+            System.out.println(broList.size());
+            if (index <= broList.size()) {
+                System.out.println(broList.get(index - 1));
+            }
+        }
+    }
+    private static boolean isBrother(String word, String target) {
+        char[] ch1 = word.toCharArray();
+        char[] ch2 = target.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for (int i = 0; i < ch1.length; i++) {
+            if (ch1[i] != ch2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
