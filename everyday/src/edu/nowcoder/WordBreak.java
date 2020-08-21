7ffd5f494142f6b4731227e6ae0759c626155596
ku-mao <1335446763@qq.com>
2020-08-21 21:15:26 +0800

    代码的优化

--- everyday/src/edu/nowcoder/WordBreak.java
+++ everyday/src/edu/nowcoder/WordBreak.java
@@ -11,21 +11,45 @@ import java.util.Set;
  * 返回true，因为"leetcode"可以被分割成"leet code".
  */
 public class WordBreak {
+//    public boolean wordBreak(String s, Set<String> dict) {
+//        if (s.length() == 0) {
+//            return false;
+//        }
+//        boolean canBreak[] = new boolean[s.length() + 1];
+//        //i 是 [1, s.length()]
+//        for (int i = 1; i <= s.length(); i++) {
+//            //j < i && f(j) = true && f(j + 1, s.length) = true
+//
+//            //[1, i]
+//            if (dict.contains(s.substring(0, i))) {
+//                canBreak[i] = true;
+//                continue;
+//            }
+//
+//            //canBreak[j] && [j + 1, i]
+//            for (int j = 1; j < i; j++) {
+//                if (canBreak[j] && dict.contains(s.substring(j, i))) {
+//                    canBreak[i] = true;
+//                    break;
+//                }
+//            }
+//        }
+//        return canBreak[s.length()];
+//    }
+
     public boolean wordBreak(String s, Set<String> dict) {
         if (s.length() == 0) {
             return false;
         }
         boolean canBreak[] = new boolean[s.length() + 1];
+        canBreak[0] = true;
         //i 是 [1, s.length()]
         for (int i = 1; i <= s.length(); i++) {
             //j < i && f(j) = true && f(j + 1, s.length) = true
 
-            //[1, i]
-            if (dict.contains(s.substring(0, i))) {
-                canBreak[i] = true;
-                continue;
-            }
-            for (int j = 1; j < i; j++) {
+
+            //canBreak[j] && [j + 1, i]
+            for (int j = 0; j < i; j++) {
                 if (canBreak[j] && dict.contains(s.substring(j, i))) {
                     canBreak[i] = true;
                     break;
