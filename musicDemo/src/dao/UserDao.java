68d9aae2e68fe017956fddf9e8d15d5605d3d9ea
ku-mao <1335446763@qq.com>
2020-09-09 23:26:17 +0800

    更新代码

--- musicDemo/src/dao/UserDao.java
+++ musicDemo/src/dao/UserDao.java
@@ -65,6 +65,34 @@ public class UserDao {
         return user;
     }
 
+    public User findByName(String username) {
+        //1.建立连接
+        Connection connection = DBUtil.getConnection();
+        //2.拼装SQL
+        String sql = "select * from user where username = ?";
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            //3.执行SQL
+            resultSet = statement.executeQuery();
+            //4.遍历结果集
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("userId"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, statement, resultSet);
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
        // UserDao userDao = new UserDao();
         //测试注册
--- musicDemo/src/servlet/RegisterServlet.java
+++ musicDemo/src/servlet/RegisterServlet.java
@@ -1,8 +1,42 @@
 package servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dao.UserDao;
+import entity.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet("/register")
+@WebServlet("/registerServlet")
 public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("application/json; charset=utf-8");
+        UserDao userDao = new UserDao();
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        Map<String, Object> returnMap = new HashMap<>();
+        if(null == username || "".equals(username)) {
+            returnMap.put("msg", false);
+        } else {
+            User user = new User();
+            user.setUsername(username);
+            user.setPassword(password);
+            User loginUser = userDao.findByName(username);
+            if (loginUser != null) {
+                returnMap.put("msg", false);
+            }else {
+                returnMap.put("msg", true);
+            }
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(resp.getWriter(), returnMap);
+    }
 }
