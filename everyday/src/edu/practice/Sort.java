23e552eb884ace7771ef6b69cbb641e3980b128c
ku-mao <1335446763@qq.com>
2020-09-17 23:41:54 +0800

    练习

--- everyday/src/edu/practice/Sort.java
+++ everyday/src/edu/practice/Sort.java
@@ -18,6 +18,30 @@ public class Sort {
             }
         }
     }
+
+    /**
+     * 插入排序
+     */
+    public void insertSort(int[] arr) {
+        for(int index = 0 ; index < arr.length -1 ; index++){ //已排好数组的最后一个元素的索引
+            int temp = arr[index+1];//待插入元素的值
+            int insert_index = index + 1;//待插入位置的索引,必须加1，因为可能会不交换
+
+            //找到待插入位置的索引
+            for(int i = index ; i >= 0 ; i-- ){
+                if(temp < arr[i]){
+                    insert_index = i;
+                }else {
+                    break;
+                }
+            }
+            //后移插入元素后面的元素
+            for(int j = index ; j >= insert_index ; j--){
+                arr[j+1] = arr[j];
+            }
+            arr[insert_index] = temp;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("请输入要排序数组的长度: ");
         Scanner scanner = new Scanner(System.in);
@@ -30,7 +54,8 @@ public class Sort {
             i++;
         }
         Sort sort = new Sort();
-        sort.bubbleSort(array);
+        //sort.bubbleSort(array);
+        sort.insertSort(array);
         for (int num : array) {
             System.out.print(num + " ");
         }
