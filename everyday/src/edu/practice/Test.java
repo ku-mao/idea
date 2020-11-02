43c96855603190a2f735692cb58d6466ef95600c
ku-mao <1335446763@qq.com>
2020-11-02 08:29:45 +0800

    练习

--- everyday/src/edu/practice/Test.java
+++ everyday/src/edu/practice/Test.java
@@ -35,36 +35,60 @@ public class Test {
 //        }
 
         //快排
-        quickSort(a);
+//        quickSort(a);
+//    }
+//    //快速排序
+//    private static void quickSort(int[] a) {
+//        int i = 0;
+//        int j = a.length - 1;
+//        quickSortHelper(a, i, j);
+//    }
+//    private static void quickSortHelper(int[] a, int i, int j) {
+//        if (i >= j) {
+//            return;
+//        }
+//        int low = i;
+//        int high = j;
+//        int position = a[i];
+//        while (low < high) {
+//            while (low < high && a[high] > position) {
+//                high--;
+//            }
+//            a[low] = a[high];
+//            while (low < high && a[low] < position) {
+//                low++;
+//            }
+//            a[high] = a[low];
+//        }
+//        a[low] = position;
+//        quickSortHelper(a, i, low - 1);
+//        quickSortHelper(a, low + 1, j);
+//    }
+
+
+        //选择排序
+        for (int i = 0; i < a.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[minIndex] > a[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(a, minIndex, i);
+        }
+
+
         for (int i : a) {
             System.out.print(i + " ");
         }
+
     }
-    //快速排序
-    private static void quickSort(int[] a) {
-        int i = 0;
-        int j = a.length - 1;
-        quickSortHelper(a, i, j);
+
+    //交换方法
+    private static void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
-    private static void quickSortHelper(int[] a, int i, int j) {
-        if (i >= j) {
-            return;
-        }
-        int low = i;
-        int high = j;
-        int position = a[i];
-        while (low < high) {
-            while (low < high && a[high] > position) {
-                high--;
-            }
-            a[low] = a[high];
-            while (low < high && a[low] < position) {
-                low++;
-            }
-            a[high] = a[low];
-        }
-        a[low] = position;
-        quickSortHelper(a, i, low - 1);
-        quickSortHelper(a, low + 1, j);
-    }
+
 }
