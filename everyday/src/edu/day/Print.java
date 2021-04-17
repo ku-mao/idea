12442e52348e45d2b52fb5f3092ab101a70901f4
ku-mao <1335446763@qq.com>
2021-04-17 14:25:52 +0800

    练习

---
+++ everyday/src/edu/day/Print.java
@@ -0,0 +1,19 @@
+package edu.day;
+
+import java.util.Scanner;
+
+public class Print {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < n - i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
