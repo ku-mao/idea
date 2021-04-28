b9a829848844f20bbd12c7ed8f7ee7b88f759c43
ku-mao <1335446763@qq.com>
2021-04-28 21:24:27 +0800

    练习

--- everyday/src/edu/day/DeleteNode.java
+++ everyday/src/edu/day/DeleteNode.java
@@ -15,11 +15,22 @@ public class DeleteNode {
         cur = node;
         node = new ListNode(3);
         cur.next = node;
-        cur = node;
-        ListNode root = deleteDuplicates(head);
-        while (root != null) {
-            System.out.print(root.val + " ");
-            root = root.next;
+//        while (head != null) {
+//            System.out.print(head.val + " ");
+//            head = head.next;
+//        }
+//        System.out.println();
+
+//        ListNode root = deleteDuplicates(head);
+//        while (root != null) {
+//            System.out.print(root.val + " ");
+//            root = root.next;
+//        }
+//        System.out.println();
+        ListNode root2 = deleteDuplicates2(head);
+        while (root2 != null) {
+            System.out.print(root2.val + " ");
+            root2 = root2.next;
         }
     }
     private static ListNode deleteDuplicates (ListNode head) {
@@ -31,5 +42,23 @@ public class DeleteNode {
             cur = cur.next;
         }
         return head;
+    }
+    private static ListNode deleteDuplicates2(ListNode head) {
+        ListNode pHead = new ListNode(-1);
+        pHead.next = head;
+        ListNode cur = pHead;
+        while (head != null && head.next != null ) {
+            if ( head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+                cur.next = head.next;
+                head = head.next;
+            } else {
+                cur = head;
+                head = head.next;
+            }
+        }
+        return pHead.next;
     }
 }
